d5a176d0061a893190b690bf06d866d0e23621a7
Edward <shawzida@gmail.com>
2014-08-21 07:33:09 +0800

    博客增加markdown,增加上传功能,增加用户页面和文章页面,增加编辑和删除功能

--- code/blog/models/post.js
+++ code/blog/models/post.js
@@ -1,4 +1,5 @@
-var mongodb = require('./db');
+var mongodb = require('./db'),
+	markdown = require('markdown').markdown;
 
 function Post(name, title, post){
 	this.name = name;
@@ -53,8 +54,8 @@ Post.prototype.save = function(callback){
 	});
 };
 
-//读取文章及相关信息
-Post.get = function(name, callback){
+//读取全部文章及相关信息
+Post.getAll = function(name, callback){
 	//打开数据库
 	mongodb.open(function(err, db){
 		if(err){
@@ -78,8 +79,135 @@ Post.get = function(name, callback){
 				if(err){
 					return callback(err);
 				}
-				console.dir(docs);
+				//解析markdown为html
+				docs.forEach(function(doc){
+					doc.post = markdown.toHTML(doc.post);
+				});
 				callback(null, docs);
+			});
+		});
+	});
+};
+
+//读取一篇文章
+Post.getOne = function(name, day, title, callback){
+	//打开数据库
+	mongodb.open(function(err, db){
+		if(err){
+			return callback(err);
+		}
+
+		//读取posts集合
+		db.collection('posts', function(err, collection){
+			if(err){
+				mongodb.close();
+				return callback(err);
+			}
+
+			//根据name、day、title进行查询
+			collection.findOne({
+				"name": name,
+				"time.day": day,
+				"title": title
+			}, function(err, doc){
+				mongodb.close();
+				if(err){
+					return callback(err);
+				}
+
+				doc.post = markdown.toHTML(doc.post);
+				callback(null, doc);
+			});
+		});	
+	});
+};
+
+//编辑文章 markdown格式
+Post.edit = function(name, day, title, callback){
+	//打开数据库
+	mongodb.open(function(err, db){
+		if(err){
+			return callback(err);
+		}
+
+		//读取posts集合
+		db.collection('posts', function(err, collection){
+			if(err){
+				mongodb.close();
+				return callback(err);
+			}
+
+			//根据name、day、title进行查询
+			collection.findOne({
+				"name": name,
+				"time.day": day,
+				"title": title
+			}, function(err, doc){
+				mongodb.close();
+				if(err){
+					return callback(err);
+				}
+				callback(null, doc);
+			});
+		});
+	});
+};
+
+//更新文章
+Post.update = function(name, day, title, post, callback){
+	//打开数据库
+	mongodb.open(function(err, db){
+		if(err){
+			return callback(err);
+		}
+		//读取posts集合
+		db.collection('posts', function(err, collection){
+			if(err){
+				mongodb.close();
+				return callback(err);
+			}
+			//更新内容
+			collection.update({
+				"name": name,
+				"time.day": day,
+				"title": title
+			}, {
+				$set: {post: post}
+			}, function(err){
+				mongodb.close();
+				if(err){
+					return callback(err);
+				}
+				callback(null);
+			});
+		});
+	});
+};
+
+//删除文章
+Post.remove = function(name, day, title, callback){
+	//打开数据库
+	mongodb.open(function(err, db){
+		if(err){
+			return callback(err);
+		}
+		//读取posts集合
+		db.collection('posts', function(err, collection){
+			if(err){
+				mongodb.close();
+				return callback(err);
+			}
+			//根据name、day、title删除文章
+			collection.remove({
+				"name": name,
+				"time.day": day,
+				"title": title
+			}, { w: 1}, function(err){
+				mongodb.close();
+				if(err){
+					return callback(err);
+				}
+				callback(null);
 			});
 		});
 	});
